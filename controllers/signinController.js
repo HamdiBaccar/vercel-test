5d10abe369a724ee538a227e0817ea3e36a21de9
Hamdi <bcr.hamdi@gmail.com>
2023-08-23 20:21:44 +0100

    sixth

--- controllers/signinController.js
+++ controllers/signinController.js
@@ -12,17 +12,17 @@ const generateSecretKey = () => {
 
 //sign-in
 exports.login = async (req, res) => {
-  const { username, password } = req.body;
+  const { email, password } = req.body;
 
   try {
-    const user = await Client.findOne({ Username: username });
+    const user = await Client.findOne({ Email: email });
     if (!user) return res.status(401).json({ error: 'Invalid credentials' });
 
     const validPassword = await bcrypt.compare(password, user.PasswordHash);
     if (!validPassword) return res.status(401).json({ error: 'Invalid credentials' });
 
     const secretKey = generateSecretKey();
-       const token = jwt.sign({ username: user.Username }, secretKey, { expiresIn: '1h' });
+       const token = jwt.sign({ email: user.Email }, secretKey, { expiresIn: '1h' });
        res.json({ token });
      } catch (error) {
        res.status(500).json({ error: 'An error occurred' });
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const signupController = require('../controllers/signupController');
 const signinController = require('../controllers/signinController');
+const signupController = require('../controllers/signupController');
 
 router.post('/signup', signupController.signup);
 router.post('/login', signinController.login);
-
 module.exports = router;
-
-
