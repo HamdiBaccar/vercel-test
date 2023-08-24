cc0ba0e794d82e815c61900413fe484d10e5d137
Hamdi <bcr.hamdi@gmail.com>
2023-08-24 13:01:49 +0100

    Seventh

--- Models/Client.js
+++ Models/Client.js
@@ -1,13 +1,12 @@
 const mongoose = require('mongoose');
 
 const ClientSchema = new mongoose.Schema({
-  ClientID: { type: Number, required: true, unique: true },
+  ClientID: { type: Number, unique: true },
   FirstName: { type: String, required: true },
   LastName: { type: String, required: true },
   Password: { type: String, required: true },
   Email: { type: String, required: true },
   Profession :{ type: String, required: true },
-  Phone_Number: { type: Number, required: true, unique: true },
 });
 
 const Client = mongoose.model('Client', ClientSchema);
